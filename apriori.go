506e4005d132d05bbfd8783dc8f94ecce0fa7d9c
Michael Valdron <michael.valdron@gmail.com>
2018-02-01 06:41:10 -0500

    Apriori algorithm improved.

--- apriori.go
+++ apriori.go
@@ -7,34 +7,126 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"bufio"
+	"sort"
+	"strings"
 )
 
-func Apriori(fname string, t_hold int) []map[string]int {
-	var k = 0
-	var fcontents []string
-	var item_set_counts []map[string]int
-	var min_supp int
+func getFreqItemsApriori(fname string, min_supp int) map[string]int {
+	f, is_open := openFile(fname)
 
-	f, err := os.Open(fname)
-	defer f.Close()
-	if err != nil {
-		fmt.Printf("Error reading file %s.\n", fname)
-		return []map[string]int{}
+	if is_open {
+		defer f.Close()
+		item_counts := make(map[string]int)
+		fs := bufio.NewScanner(f)
+		keys := []string{}
+
+		for fs.Scan() {
+			basket := strings.Split(strings.TrimSuffix(fs.Text(), " "), " ")
+			for _, item := range basket {
+				if len(keys) == 0 || !checkIn(item, keys) {
+					keys = append(keys, item)
+					item_counts[item] = 1
+				} else {
+					item_counts[item] += 1
+				}
+			}
+		}
+
+		filterItemsets(item_counts, min_supp)
+
+		return item_counts
+	} else {
+		return map[string]int{}
 	}
-	fcontents = readLines(f)
-	min_supp = t_hold
-	item_set_counts = append(item_set_counts, getUniqueItems(fcontents, min_supp))
-	fcontents = nil
+}
 
-	for len(item_set_counts[k]) > 0 && k < 2 {
-		k_item_sets := getKeyStr(item_set_counts[k])
-		init_item_sets := getKeyStr(item_set_counts[0])
-		candidate_item_set := getBaskets(k_item_sets, init_item_sets)
-		item_set_counts = append(item_set_counts, getFreqTuples(f, candidate_item_set, min_supp))
+func getBasketsApriori(k_items []string, init_items []string, k int) []string {
+	var baskets []string
+	if k == 2 {
+		for i, k_item_set := range k_items {
+			for j := i; j < len(init_items); j++ {
+				if !checkIn(init_items[j], strings.Split(k_item_set, ",")) {
+					sel_item_set := append(strings.Split(k_item_set, ","), init_items[j])
+					sort.Strings(sel_item_set)
+					if !checkIn(strings.Join(sel_item_set, ","), baskets) {
+						baskets = append(baskets, strings.Join(sel_item_set, ","))
+					}
+				}
+			}
+		}
+	} else {
+		for _, k_item_set := range k_items {
+			for _, item := range init_items {
+				if !checkIn(item, strings.Split(k_item_set, ",")) {
+					sel_item_set := append(strings.Split(k_item_set, ","), item)
+					sort.Strings(sel_item_set)
+					if !checkIn(strings.Join(sel_item_set, ","), baskets) {
+						baskets = append(baskets, strings.Join(sel_item_set, ","))
+					}
+				}
+			}
+		}
+	}
+
+	return baskets
+}
+
+func getFreqTuplesApriori(fname string, tuples []string, min_supp int) map[string]int {
+	f, is_open := openFile(fname)
+
+	if is_open {
+		defer f.Close()
+		itemset_counts := make(map[string]int)
+		keys := []string{}
+
+		for _, tuple := range tuples {
+			f.Seek(0, 0)
+			fs := bufio.NewScanner(f)
+			items := strings.Split(tuple, ",")
+
+			for fs.Scan() {
+				basket := strings.Split(strings.TrimSuffix(fs.Text(), " "), " ")
+				similar_count := 0
+				for _, item := range items {
+					if checkIn(item, basket) {
+						similar_count += 1
+					}
+				}
+				if similar_count == len(items) {
+					if !checkIn(tuple, keys) {
+						keys = append(keys, tuple)
+						itemset_counts[tuple] = 1
+					} else {
+						itemset_counts[tuple] += 1
+					}
+				}
+			}
+		}
+
+		filterItemsets(itemset_counts, min_supp)
+
+		return itemset_counts
+	} else {
+		return map[string]int{}
+	}
+}
+
+func Apriori(fname string, t_hold float32) []map[string]int {
+	k := 0
+	n := float32(getFileLength(fname))
+	min_supp := int(t_hold * n)
+	var itemset_counts []map[string]int
+
+	itemset_counts = append(itemset_counts, getFreqItemsApriori(fname, min_supp))
+
+	for len(itemset_counts[k]) > 0 && k < 2 {
+		k_item_sets := getKeyStr(itemset_counts[k])
+		init_item_sets := getKeyStr(itemset_counts[0])
+		candidate_item_set := getBasketsApriori(k_item_sets, init_item_sets, k)
+		itemset_counts = append(itemset_counts, getFreqTuplesApriori(fname, candidate_item_set, min_supp))
 		k += 1
 	}
 
-	return item_set_counts
+	return itemset_counts
 }
