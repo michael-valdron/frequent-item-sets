9ad806f706e149ce42924e7268074986515a8aae
Michael Valdron <michael.valdron@gmail.com>
2018-02-04 11:07:39 -0500

    General Apriori and PCY functions moved.  Procedures improved.

--- apriori.go
+++ apriori.go
@@ -7,109 +7,32 @@
 package main
 
 import (
-	"bufio"
 	"sort"
 	"strings"
 )
 
 func getFreqItemsApriori(fname string, min_supp int) map[string]int {
-	f, is_open := openFile(fname)
-
-	if is_open {
-		defer f.Close()
-		item_counts := make(map[string]int)
-		fs := bufio.NewScanner(f)
-		keys := []string{}
-
-		for fs.Scan() {
-			basket := strings.Split(strings.TrimSuffix(fs.Text(), " "), " ")
-			for _, item := range basket {
-				if len(keys) == 0 || !checkIn(item, keys) {
-					keys = append(keys, item)
-					item_counts[item] = 1
-				} else {
-					item_counts[item] += 1
-				}
-			}
-		}
-
-		filterItemsets(item_counts, min_supp)
-
-		return item_counts
-	} else {
-		return map[string]int{}
-	}
+	itemset, _ := getFreqItems(fname, min_supp, false, 0)
+	return itemset
 }
 
-func getBasketsApriori(k_items []string, init_items []string, k int) []string {
+func getBasketsApriori(k_items []string, init_items []string) []string {
 	var baskets []string
-	if k == 2 {
-		for i, k_item_set := range k_items {
-			for j := i; j < len(init_items); j++ {
-				if !checkIn(init_items[j], strings.Split(k_item_set, ",")) {
-					sel_item_set := append(strings.Split(k_item_set, ","), init_items[j])
-					sort.Strings(sel_item_set)
-					if !checkIn(strings.Join(sel_item_set, ","), baskets) {
-						baskets = append(baskets, strings.Join(sel_item_set, ","))
-					}
-				}
-			}
-		}
-	} else {
-		for _, k_item_set := range k_items {
-			for _, item := range init_items {
-				if !checkIn(item, strings.Split(k_item_set, ",")) {
-					sel_item_set := append(strings.Split(k_item_set, ","), item)
-					sort.Strings(sel_item_set)
-					if !checkIn(strings.Join(sel_item_set, ","), baskets) {
-						baskets = append(baskets, strings.Join(sel_item_set, ","))
-					}
+	for _, k_item_set_str := range k_items {
+		k_item_set := strings.Split(k_item_set_str, TUPLE_SEP)
+		for _, item := range init_items {
+			if !checkIn(item, k_item_set) {
+				sel_item_set := append(k_item_set, item)
+				sort.Strings(sel_item_set)
+				sel_item_set_str := strings.Join(sel_item_set, TUPLE_SEP)
+				if !checkIn(sel_item_set_str, baskets) {
+					baskets = append(baskets, sel_item_set_str)
 				}
 			}
 		}
 	}
 
 	return baskets
-}
-
-func getFreqTuplesApriori(fname string, tuples []string, min_supp int) map[string]int {
-	f, is_open := openFile(fname)
-
-	if is_open {
-		defer f.Close()
-		itemset_counts := make(map[string]int)
-		keys := []string{}
-
-		for _, tuple := range tuples {
-			f.Seek(0, 0)
-			fs := bufio.NewScanner(f)
-			items := strings.Split(tuple, ",")
-
-			for fs.Scan() {
-				basket := strings.Split(strings.TrimSuffix(fs.Text(), " "), " ")
-				similar_count := 0
-				for _, item := range items {
-					if checkIn(item, basket) {
-						similar_count += 1
-					}
-				}
-				if similar_count == len(items) {
-					if !checkIn(tuple, keys) {
-						keys = append(keys, tuple)
-						itemset_counts[tuple] = 1
-					} else {
-						itemset_counts[tuple] += 1
-					}
-				}
-			}
-		}
-
-		filterItemsets(itemset_counts, min_supp)
-
-		return itemset_counts
-	} else {
-		return map[string]int{}
-	}
 }
 
 func Apriori(fname string, t_hold float32) []map[string]int {
@@ -123,8 +46,9 @@ func Apriori(fname string, t_hold float32) []map[string]int {
 	for len(itemset_counts[k]) > 0 && k < 2 {
 		k_item_sets := getKeyStr(itemset_counts[k])
 		init_item_sets := getKeyStr(itemset_counts[0])
-		candidate_item_set := getBasketsApriori(k_item_sets, init_item_sets, k)
-		itemset_counts = append(itemset_counts, getFreqTuplesApriori(fname, candidate_item_set, min_supp))
+		candidate_item_set := getBasketsApriori(k_item_sets, init_item_sets)
+		itemset_counts = append(itemset_counts, getFreqTuples(fname, candidate_item_set, min_supp))
+
 		k += 1
 	}
 
