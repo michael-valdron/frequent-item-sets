97d64f7dacb1e01745992bdf84f706321a1b223a
Michael Valdron <michael.valdron@gmail.com>
2018-02-25 18:50:10 -0500

    Print time taken for every pass added.

--- apriori.go
+++ apriori.go
@@ -6,12 +6,18 @@
 */
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func Apriori(fname string, t_hold float32) ([]map[int][]int, []map[int]int) {
 	k := 0
 	var itemsets []map[int][]int
 	var counts []map[int]int
 
 	// Get Items First Pass -------------------------------------------------------------------------------------
+	start_time := time.Now()
 	c_item_counts, min_supp, _, _ := GetFreqItems(fname, t_hold, false)
 
 	itemsets = []map[int][]int{}
@@ -21,10 +27,18 @@ func Apriori(fname string, t_hold float32) ([]map[int][]int, []map[int]int) {
 
 	counts = append(counts, c_item_counts)
 
+	finish_time := time.Now()
+
+	fmt.Printf("Pass %d took: %d minutes and %d seconds.\n",
+		(k + 1),
+		int(finish_time.Sub(start_time).Minutes()),
+		(int(finish_time.Sub(start_time).Seconds()) % 60))
+
 	// ----------------------------------------------------------------------------------------------------------
 
 	// Calculate Itemsets Second to Finite Pass -----------------------------------------------------------------
 	for len(itemsets[k]) > 0 && k < 2 {
+		start_time = time.Now()
 		c_itemsets, c_itemset_counts := GenTuples(itemsets, k, map[int]bool{}, 0, false)
 		c_itemsets, c_itemset_counts = GetFreqTuples(c_itemsets, c_itemset_counts, fname, k, min_supp, map[int]bool{}, 0)
 
@@ -33,8 +47,12 @@ func Apriori(fname string, t_hold float32) ([]map[int][]int, []map[int]int) {
 		FilterItemsets(itemsets, c_itemsets, c_itemset_counts, min_supp, k)
 
 		counts = append(counts, c_itemset_counts)
-
+		finish_time = time.Now()
 		k++
+		fmt.Printf("Pass %d took: %d minutes and %d seconds.\n",
+			(k + 1),
+			int(finish_time.Sub(start_time).Minutes()),
+			(int(finish_time.Sub(start_time).Seconds()) % 60))
 	}
 	if len(itemsets[k]) == 0 {
 		itemsets = itemsets[0:k]
--- pcy.go
+++ pcy.go
@@ -7,7 +7,9 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"time"
 )
 
 func MulHashFunc(values []int, m int) int {
@@ -46,6 +48,7 @@ func PCY(fname string, t_hold float32) ([]map[int][]int, []map[int]int) {
 	var counts []map[int]int
 
 	// Get Items First Pass -------------------------------------------------------------------------------------
+	start_time := time.Now()
 	c_item_counts, min_supp, n_bins, itemsets_bitmap := GetFreqItems(fname, t_hold, true)
 	itemsets = []map[int][]int{}
 	itemsets = append(itemsets, make(map[int][]int))
@@ -54,10 +57,18 @@ func PCY(fname string, t_hold float32) ([]map[int][]int, []map[int]int) {
 
 	counts = append(counts, c_item_counts)
 
+	finish_time := time.Now()
+
+	fmt.Printf("Pass %d took: %d minutes and %d seconds.\n",
+		(k + 1),
+		int(finish_time.Sub(start_time).Minutes()),
+		(int(finish_time.Sub(start_time).Seconds()) % 60))
+
 	// ----------------------------------------------------------------------------------------------------------
 
 	// Calculate Itemsets Second to Finite Pass -----------------------------------------------------------------
 	for len(itemsets[k]) > 0 && k < 2 {
+		start_time = time.Now()
 		c_itemsets, c_itemset_counts := GenTuples(itemsets, k, itemsets_bitmap, n_bins, true)
 		c_itemsets, c_itemset_counts = GetFreqTuples(c_itemsets, c_itemset_counts, fname, k, min_supp, itemsets_bitmap, n_bins)
 
@@ -66,8 +77,12 @@ func PCY(fname string, t_hold float32) ([]map[int][]int, []map[int]int) {
 		FilterItemsets(itemsets, c_itemsets, c_itemset_counts, min_supp, k)
 
 		counts = append(counts, c_itemset_counts)
-
+		finish_time = time.Now()
 		k++
+		fmt.Printf("Pass %d took: %d minutes and %d seconds.\n",
+			(k + 1),
+			int(finish_time.Sub(start_time).Minutes()),
+			(int(finish_time.Sub(start_time).Seconds()) % 60))
 	}
 	if len(itemsets[k]) == 0 {
 		itemsets = itemsets[0:k]
